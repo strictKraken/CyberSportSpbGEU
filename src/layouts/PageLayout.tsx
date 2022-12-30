e2d2899470ddb4619fbb6ec34a00cba8f00c76de
Степан Лукьянов <stefan@Stepans-MacBook-Air.local>
2022-12-30 16:34:54 +0300

    feat: work on page teams

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -5,9 +5,7 @@ type Props = {
 const PageLayout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <div className="pt-[84px] md:pt-[116px] container mx-auto max-w-[1360px] px-5">
-        {children}
-      </div>
+      <div className="pt-[84px] md:pt-[116px]">{children}</div>
       <div className="w-full h-[78px] min-h-[78px] bg-gradient-to-b from-shadowFooter" />
     </>
   );
---
+++ src/pages/teams/[id].tsx
@@ -0,0 +1,51 @@
+import { NextPage, GetStaticProps } from "next";
+import Image from "next/image";
+import { Teams } from "../../types/globalTypes";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const teams: Teams[] = [];
+  return {
+    props: {
+      teams,
+    },
+  };
+};
+
+const SectionTitle = () => {
+  return (
+    <div className="w-full h-[265px] ">{/* <Image src={} ></Image> */}</div>
+  );
+};
+
+const TeamBlock = ({ title, members }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>
+        {members.map((item) => (
+          <></>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const SectionTeams = ({ teams }) => {
+  return (
+    <>
+      {teams?.map((item) => (
+        <TeamBlock title={item.title} members={item.members} />
+      ))}
+    </>
+  );
+};
+
+const TeamPage: NextPage = ({ teams }) => {
+  return (
+    <>
+      <SectionTeams teams={teams} />
+    </>
+  );
+};
+
+export default TeamPage;
