d326340225075ef65b02bfd099499c09b3bb56a1
strictKraken <StefanStark77@gmail.com>
2023-01-01 19:14:05 +0300

    start landing page twitch

--- src/pages/stream.tsx
+++ src/pages/stream.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import Image from "next/image";
 
 const Info = () => (
   <section className="container px-[60px]">
@@ -6,7 +6,24 @@ const Info = () => (
   </section>
 );
 
-const Stream: NextPage = () => (
+const TwitchPage = () => {
+  return (
+    <section>
+      <div className="flex gap-[24px]">
+        <h2 className="section-title">следи за нами на twitch</h2>
+        <div className="h-[54px] w-[56px]">
+          <Image src={"/icons/logoTwitchArrow.svg"} layout="fill" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Iframe = () => {
+  return <></>;
+};
+
+const Stream: React.FC = () => (
   <>
     <div className="min-h-screen bg-main-bg">
       <Info />
